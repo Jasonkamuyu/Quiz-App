9822d98a193a7994fad75c885b43c76960652c95
unknown <jasonkamuyu@gmail.com>
2023-03-03 17:55:52 +0300

    New QuizApp Project

---
+++ lib/controllers/question_paper/data_uploader.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class DataUploader extends GetxController {
+  @override
+  void onReady() {
+    uploadData();
+    super.onReady();
+  }
+
+  void uploadData() {}
+}
---
+++ lib/data_uploaded_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DataUploadScreen extends StatelessWidget {
+  const DataUploadScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: AboutDialog()),
+    );
+  }
+}

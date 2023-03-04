6f57f1aabc000b91d58c978721253e27b9b25059
unknown <jasonkamuyu@gmail.com>
2023-03-04 12:30:53 +0300

    Added Questions to Firebase

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_quiz_app/firebase_ref/references.dart';
+import 'package:flutter_quiz_app/models/question_paper_model.dart';
 import 'package:get/get.dart';
 
 class DataUploader extends GetxController {
@@ -7,5 +14,39 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
-  void uploadData() {}
+  Future<void> uploadData() async {
+    final firestore = FirebaseFirestore.instance;
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+
+    //load json file and print path
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/paper") && path.contains(".json"))
+        .toList();
+
+    List<QuestionPaperModel> questionPapers = [];
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+    }
+    //print('Items Number ${questionPapers[0].id}');
+
+    var batch = firestore.batch();
+
+    for (var paper in questionPapers) {
+      batch.set(questionPaperRF.doc(paper.id), {
+        'title': paper.title,
+        'image_url': paper.imageUrl,
+        'description': paper.description,
+        'time_second': paper.timeSeconds,
+        'questions_count': paper.questions == null ? 0 : paper.questions!.length
+      });
+    }
+
+    await batch.commit();
+  }
 }
--- lib/data_uploaded_screen.dart
+++ lib/data_uploaded_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/controllers/question_paper/data_uploader.dart';
+import 'package:get/get.dart';
 
 class DataUploadScreen extends StatelessWidget {
-  const DataUploadScreen({super.key});
+  DataUploadScreen({super.key});
+
+  DataUploader controller = Get.put(DataUploader());
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: AboutDialog()),
+      body: Center(child: Text("Uploading")),
     );
   }
 }
